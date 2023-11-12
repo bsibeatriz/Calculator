d26e02a7c7b6aed9e9c7b39ac49bb36e8cfeb785
bsibeatriz <beatrizmaarqueszo@gmail.com>
2023-11-12 18:08:01 -0300

    :)

--- EIRP/script.js
+++ EIRP/script.js
@@ -1,9 +1,11 @@
 function calcular() {
-    const potenciaTransmissao = parseFloat(document.getElementById('transmissionPower').value);
+    const potenciaTransmissaoMW = parseFloat(document.getElementById('transmissionPower').value);
     const ganhoAntena = parseFloat(document.getElementById('antennaGain').value);
     const perdasCabo = parseFloat(document.getElementById('cableLoss').value);
 
-    const eirp = potenciaTransmissao + ganhoAntena - perdasCabo;
+    const potenciaTransmissaoDBm = 10 * Math.log10(potenciaTransmissaoMW);
+
+    const eirp = potenciaTransmissaoDBm + ganhoAntena - perdasCabo;
 
     const resultadoElement = document.getElementById('resultado');
     resultadoElement.textContent = `EIRP (dBm): ${eirp.toFixed(2)} dBm`;
--- Fresnel Zone/script.js
+++ Fresnel Zone/script.js
@@ -2,10 +2,16 @@ function calcular() {
     const distTransmissorObstaculo = parseFloat(document.getElementById('distanceTransmitterObstacle').value);
     const distReceptorObstaculo = parseFloat(document.getElementById('distanceReceiverObstacle').value);
     const distancia = parseFloat(document.getElementById('distance').value);
+    const alturaAntenas = parseFloat(document.getElementById('antennaHeight').value);
     const frequencia = parseFloat(document.getElementById('frequency').value);
 
-    const raioFresnel = 550 * Math.sqrt((distTransmissorObstaculo * distReceptorObstaculo) / (distancia * frequencia));
+    const raioFresnel = 72.6 * Math.sqrt((distTransmissorObstaculo * distReceptorObstaculo) / (distancia * frequencia));
 
-    const resultadoElement = document.getElementById('resultado');
-    resultadoElement.textContent = `Raio da Zona de Fresnel (m): ${raioFresnel.toFixed(2)} m`;
+    
+    if (frequencia < 3000) {
+        const raioFresnel60 = 0.6 * raioFresnel;
+        resultadoElement.textContent = `Raio da Zona de Fresnel (m): ${raioFresnel60.toFixed(2)} m (60% do raio para frequências < 3 GHz)`;
+    } else {
+        resultadoElement.textContent = `Raio da Zona de Fresnel (m): ${raioFresnel.toFixed(2)} m`;
+    }
 }
